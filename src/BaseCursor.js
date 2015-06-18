9b2e54a1bb0d7372d8dccb014f6b920a5cc53dd4
Lukas Bünger <lukas.buenger@fixxpunkt.ch>
2015-06-18 09:27:52 +0200

    Remove class properties (for now)

--- src/BaseCursor.js
+++ src/BaseCursor.js
@@ -4,13 +4,6 @@ let Iterator = Iterable.Iterator;
 
 export default {
 
-	size: undefined,
-	_rootData: undefined,
-	_keyPath: undefined,
-	_onChange: undefined,
-	_api: undefined,
-	sharedOptions: undefined,
-
 	deref(notSetValue = undefined) {
 		return this._rootData.getIn(this._keyPath, notSetValue);
 	},
